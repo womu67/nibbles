153d3932b2f658d433d6f2a1437733f3155e6f78
womu67 <Wolfgang.Mutsch@gmx.de>
2026-01-16 17:32:24 +0100

    Schritt5d1

--- python-app/app.py
+++ python-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hallo Welt! Meine erste Python-App läuft 🚀 juchu20"
+    return "Hallo Welt! Meine erste Python-App läuft 🚀 juchu21"
 
 
 if __name__ == "__main__":
