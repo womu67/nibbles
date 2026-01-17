6ac7347586ad2994842d532b67c18047f6f2ba60
womu67 <Wolfgang.Mutsch@gmx.de>
2026-01-17 11:08:46 +0100

    Refactor 1b

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hallo Welt! Meine erste Python-App läuft 🚀 juchu27"
+    return "Hallo Welt! Meine erste Python-App läuft 🚀 juchu28"
 
 
 if __name__ == "__main__":
